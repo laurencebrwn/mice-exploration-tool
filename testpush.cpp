615896e41cc220e780c40824758764d39b2202b0
1868530 <18685030@students.lincoln.ac.uk>
2019-11-25 15:56:52 +0000

    first test push to repo

---
+++ testpush.cpp
@@ -0,0 +1,20 @@
+//Test github program fo command line practice - please delete when required
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+string hello = "hello";
+
+cout << hello << endl; 
+
+//add you new lines below to test pushing etc. Let's see what story we can create !!
+
+}
+
+
+

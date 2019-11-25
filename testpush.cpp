f5ee11654bde4ddc1906017bd9f7463032fbc13a
Laurence <snowsnooks@gmail.com>
2019-11-25 18:13:35 +0000

    Laurences first commit to testpush file

--- testpush.cpp
+++ testpush.cpp
@@ -11,6 +11,7 @@ int main()
 string hello = "hello";
 
 cout << hello << endl; 
+cout << "I am a mouse" << endl;
 
 //add you new lines below to test pushing etc. Let's see what story we can create !!
 

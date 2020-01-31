e29059f01bef29d7763076a1a071a0d077971974
WJEdgar <wj.edgar@yahoo.co.uk>
2020-01-31 20:51:48 +0000

    Begun work on removing unneeded tags and data

--- JSONReader.cs
+++ JSONReader.cs
@@ -6,7 +6,7 @@ namespace JSON_Converter
 {
     class JSONRead
     {
-        static void readIn(string Filepath)
+        static string[][] readIn(string Filepath)
         {
             ArrayList StringLists = new ArrayList();//ArrayList used to account for input size
             StreamReader Reader = new StreamReader(Filepath);//Initiates reader for file location
@@ -32,11 +32,21 @@ namespace JSON_Converter
             int i = UnSortedArray.Length;//gets length
             string[][] SortedArray = new string[i][1];//generates a 2d array 
             string[] Split = new string[2];
-            for(int i = 0; i<UnSortedArray.Length; i++){
-                Split = UnSortedArray[i].Split(":");
-                SortedArray[i][0] = Split[0];
+            for(int i = 0; i<UnSortedArray.Length; i++){//iterates through
+                Split = UnSortedArray[i].Split(":");//Splits it based upon the : character
+                SortedArray[i][0] = Split[0];//Stored in new array
                 SortedArray[i][1] = Split[1];
             }
+
+            return SortedArray;
+        }
+
+        static string[][] RemoveUneeded(string[][] Input){
+            string[] NeededTags = {"biological_sample_id","phenotyping_center","date_of_birth","sex","age_in_weeks","weight (g)","biological_sample_group",
+            "gene_symbol","gene_accession_id","zygosity", "parameter_name", "observation_type","category","download_url","jpeg_url","biological_sample_id",
+            "parameter_name"};
+            int x_length = Input.GetLength(0);
+            int y_length = Input.GetLength(1);
         }
         static void Main(string[] args)
         {

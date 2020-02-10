1f03311ded63ac6698617e051d33ab88fba0c7c3
18685030 <GarryClawson>
2020-02-10 18:46:51 +0000

    Image Buttons for Query

--- Pages/dicomImageToGrid.cshtml.cs
+++ Pages/dicomImageToGrid.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace helloWorld.Pages
 {
@@ -17,9 +18,31 @@ namespace helloWorld.Pages
             _logger = logger;
         }
 
+        public string Message { get; set; }
+        public object HiddenField1 { get; set; }
+        public object HiddenField2 { get; set; }
+
         public void OnGet()
         {
 
+            Message = "Hello World";
+
+            List<string> strList = new List<string>();
+            strList.Add("str1");
+            strList.Add("str2");
+            strList.Add("str3");
+
+            List<int> intList = new List<int>();
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+
+
+            HiddenField1 = Newtonsoft.Json.JsonConvert.SerializeObject(strList);
+            HiddenField2 = Newtonsoft.Json.JsonConvert.SerializeObject(intList);
         }
+
+
     }
+    
 }

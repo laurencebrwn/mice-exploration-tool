e3b487608ed648abfced123da31c8ec313d42224
1868530 <18685030@students.lincoln.ac.uk>
2020-02-14 13:01:50 +0000

    Removed repeated functions and added new View All button

--- Pages/dicomImageToGrid.cshtml.cs
+++ Pages/dicomImageToGrid.cshtml.cs
@@ -23,173 +23,50 @@ namespace helloWorld.Pages
         public void OnGet()
         {
 
-            // Opens a db connection using localhost database connection.Could also have used 127.0.0.1
-            String str = @"server=localhost; database=MICE; userid=root; password=TSEGroup34;";
-            MySqlConnection conn = null;
-            MySqlDataReader reader = null;
-
-            try //To open localhost database and present a query
-            {
-                //Create a object with 'str' connection values passed. This uses the inbuilt library of MySql which is required
-                conn = new MySqlConnection(str);
-                conn.Open(); //opens the database connection
-                Console.WriteLine("Localhost MySQL Database Connected"); //If the database opens it presents this messsge. 
-
-                //Assigns a query to cmdText which will be passed to the MySql database
-                //Returns URLs of all female mice where the patient_id's match (because data is in 2 tables)
-
-                //string cmdText = @"
-                //SELECT urlString FROM url WHERE patient_id IN 
-                //(SELECT patient_id FROM mice WHERE patient_sex = 'F');";
-
-                //shows all images in database via URLs
-                string cmdText = @"
-                SELECT urlString FROM MICE.url;";
-
-
-                //Creates object and passes all returned values to it
-                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
-                reader = cmd.ExecuteReader();
-
-                //Loops through the returned values and writes them to an array that will be passed to client side
-                int i = 0;
-                string[] arrayURL = new string[25];
-                while (reader.Read())
-                {
-                    //Console.WriteLine(reader.GetString(0));
-                    arrayURL[i] = reader.GetString(0);
-                    i++;
-                }
-
-                ViewData["DICOMArrayList"] = arrayURL;
-
-            }
-            catch (MySqlException errorMessage) //Prints exception if the connection cannot be opened (wrong password etc)
-            {
-                Console.WriteLine(errorMessage);
-            }
-            finally //Once the try-ctach block is complete the connection is closed
-            {
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
-
+            //Shows all images in the databse as a defult GET request
+            OnPostViewAll();
         }
 
-
-        //shows only female samples
+        //show only female samples
         public IActionResult OnPostFemales()
         {
 
-            // Opens a db connection using localhost database connection.Could also have used 127.0.0.1
-            String str = @"server=localhost; database=MICE; userid=root; password=TSEGroup34;";
-            MySqlConnection conn = null;
-            MySqlDataReader reader = null;
-
-            try //To open localhost database and present a query
-            {
-                //Create a object with 'str' connection values passed. This uses the inbuilt library of MySql which is required
-                conn = new MySqlConnection(str);
-                conn.Open(); //opens the database connection
-                Console.WriteLine("Localhost MySQL Database Connected"); //If the database opens it presents this messsge. 
-
-                //Assigns a query to cmdText which will be passed to the MySql database
-                //Returns URLs of all female mice where the patient_id's match (because data is in 2 tables)
-                string cmdText = @"
+            string cmdText = @"
                 SELECT urlString FROM url WHERE patient_id IN 
                 (SELECT patient_id FROM mice WHERE patient_sex = 'F');";
 
-                //Creates object and passes all returned values to it
-                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
-                reader = cmd.ExecuteReader();
-
-                //Loops through the returned values and writes them to an array that will be passed to client side
-                int i = 0;
-                string[] arrayURL = new string[25];
-                while (reader.Read())
-                {
-                    //Console.WriteLine(reader.GetString(0));
-                    arrayURL[i] = reader.GetString(0);
-                    i++;
-                }
-
-                ViewData["DICOMArrayList"] = arrayURL;
-
-            }
-            catch (MySqlException errorMessage) //Prints exception if the connection cannot be opened (wrong password etc)
-            {
-                Console.WriteLine(errorMessage);
-            }
-            finally //Once the try-ctach block is complete the connection is closed
-            {
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
+            MySqlConnection(cmdText);
 
             return Page();
-
         }
 
-        //shows only male samples
+        //show only male examples
         public IActionResult OnPostMales()
         {
 
-            // Opens a db connection using localhost database connection.Could also have used 127.0.0.1
-            String str = @"server=localhost; database=MICE; userid=root; password=TSEGroup34;";
-            MySqlConnection conn = null;
-            MySqlDataReader reader = null;
-
-            try //To open localhost database and present a query
-            {
-                //Create a object with 'str' connection values passed. This uses the inbuilt library of MySql which is required
-                conn = new MySqlConnection(str);
-                conn.Open(); //opens the database connection
-                Console.WriteLine("Localhost MySQL Database Connected"); //If the database opens it presents this messsge. 
-
-                //Assigns a query to cmdText which will be passed to the MySql database
-                //Returns URLs of all female mice where the patient_id's match (because data is in 2 tables)
-                string cmdText = @"
+            string cmdText = @"
                 SELECT urlString FROM url WHERE patient_id IN 
                 (SELECT patient_id FROM mice WHERE patient_sex = 'M');";
 
-                //Creates object and passes all returned values to it
-                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
-                reader = cmd.ExecuteReader();
-
-                //Loops through the returned values and writes them to an array that will be passed to client side
-                int i = 0;
-                string[] arrayURL = new string[25];
-                while (reader.Read())
-                {
-                    //Console.WriteLine(reader.GetString(0));
-                    arrayURL[i] = reader.GetString(0);
-                    i++;
-                }
-
-                ViewData["DICOMArrayList"] = arrayURL;
-
-            }
-            catch (MySqlException errorMessage) //Prints exception if the connection cannot be opened (wrong password etc)
-            {
-                Console.WriteLine(errorMessage);
-            }
-            finally //Once the try-ctach block is complete the connection is closed
-            {
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-            }
+            MySqlConnection(cmdText);
 
             return Page();
         }
 
-        //shows only male samples
-        public IActionResult OnPostReset()
+        //show all images in MySql database
+        public IActionResult OnPostViewAll()
+        {
+
+            string cmdText = @"
+            SELECT urlString FROM MICE.url;";
+
+            MySqlConnection(cmdText);
+
+            return Page();
+        }
+
+        //Main functon that connects to the ySql server and returns the reuqired URLs
+        public void MySqlConnection(string connection)
         {
 
             // Opens a db connection using localhost database connection.Could also have used 127.0.0.1
@@ -197,6 +74,8 @@ namespace helloWorld.Pages
             MySqlConnection conn = null;
             MySqlDataReader reader = null;
 
+            string cmdText = connection;
+
             try //To open localhost database and present a query
             {
                 //Create a object with 'str' connection values passed. This uses the inbuilt library of MySql which is required
@@ -206,8 +85,8 @@ namespace helloWorld.Pages
 
                 //Assigns a query to cmdText which will be passed to the MySql database
                 //Returns URLs of all female mice where the patient_id's match (because data is in 2 tables)
-                string cmdText = @"
-                SELECT urlString FROM MICE.url;";
+                //string cmdText = @"
+                //SELECT urlString FROM MICE.url;";
 
                 //Creates object and passes all returned values to it
                 MySqlCommand cmd = new MySqlCommand(cmdText, conn);
@@ -238,7 +117,6 @@ namespace helloWorld.Pages
                 }
             }
 
-            return Page();
         }
     }
 

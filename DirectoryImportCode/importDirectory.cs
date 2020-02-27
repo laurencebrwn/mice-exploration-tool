17f4d93dad3069d25e17c75e4193258e1391f79c
WJEdgar <wj.edgar@yahoo.co.uk>
2020-02-27 15:30:01 +0000

    Removed testing code

--- DirectoryImportCode/importDirectory.cs
+++ DirectoryImportCode/importDirectory.cs
@@ -1,10 +1,11 @@
+  
 using System;
 using System.IO;
 using System.Collections.Generic;
 
 namespace DirectoryImportCode{
-    class ImportDirectory {
-
+    class ImportDirectory{
+        
         private string DirecotryPath;
         ImportDirectory(string Directory)
         {
@@ -12,56 +13,45 @@ namespace DirectoryImportCode{
             FindFiles();
         }
 
-        public void FindFiles() {
+        public List<Image> FindFiles(){
             string[] Dir = Directory.GetFiles(@DirecotryPath);//Allows for copy paste filepaths using Override.
             List<string> ImagePaths = new List<string>();//creates a list of filepaths
             List<string> TagPaths = new List<string>();//creates a list of filepaths
-            foreach (string file in Dir) {//goes through directory finding all files
+            foreach(string file in Dir){//goes through directory finding all files
                 string ext = Path.GetExtension(file);
                 Console.WriteLine(file);
-                if (ext != ".txt" || ext != ".json") {//sorts dependent upont file type
-                    ImagePaths.Add(file);
-                } else {
-                    TagPaths.Add(file);
-                }
-            }
-            List<Image> Images = SortImage(ImagePaths);//Sorts images of the same mouse into record.
-                                                       //From here pass to code for database step.  
-
-            foreach (Image mouse in Images) {
-                List<string> files = mouse.GetImages();
-                foreach (string im in files) {
-                    Console.WriteLine(im);
-                }
-            }
+                 if( ext != ".txt" || ext !=".json"){//sorts dependent upont file type
+                     ImagePaths.Add(file);
+                 }else{
+                    TagPaths.Add(file); 
+                 }
+            }  
+             List<Image> Images = SortImage(ImagePaths);//Sorts images of the same mouse into record.
+             return Images;//returns a list of the Image object
+             
         }
 
-        public List<Image> SortImage(List<string> Files) {
+        public List<Image> SortImage(List<string> Files){
             List<string> Filepaths = Files;
             List<Image> Images = new List<Image>();
-            foreach (string File in Filepaths) {
-                Console.WriteLine("a");
-                List<string> files = new List<string>();
-                Console.WriteLine("b");
+            foreach(string File in Filepaths){
+                List<string>files = new List<string>();
                 //string id = GetID(Filepaths[0]);
                 files.Add(File);
                 Image Mouse = new Image();
                 Mouse.AddImage(files);
                 Images.Add(Mouse);
-                Console.WriteLine("e");
+
             }
-            // for this step you need to match substring after a _ and then remove from List. 
-            //4. Continue until no more matching is done, removing all the matching from list.
-            //5. Repeat until list empty
             return Images;
         }
 
-        string GetID(string filepath) {
+        string GetID(string filepath){
             int start = 0;
             string FileName = Path.GetFileNameWithoutExtension(filepath);
-            for (int i = 0; i < FileName.Length; i++) {
-                if (FileName[i] == '_') {
-                    start = i + 1;
+            for(int i = 0; i<FileName.Length; i++){
+                if(FileName[i] == '_'){
+                    start = i+1;
                     break;
                 }
             }
@@ -69,17 +59,8 @@ namespace DirectoryImportCode{
             return ID;
         }
 
-        void SortText() {//This method will handle all imported JSON files.
-
-        }
-        public static void Main(string[] args){
-            Console.WriteLine("Please enter the address of your directory");
-            string dirPath = Console.ReadLine();
-            ImportDirectory imp = new ImportDirectory(dirPath);
-            Console.ReadLine();
-
+        void SortText(){//This method will handle all imported JSON files.
 
         }
     }
 }
-    

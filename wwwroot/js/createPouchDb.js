51359b9303dc3c731f1f510bfbc5977b60c137b4
Laurence Brown <snowsnooks@gmail.com>
2020-03-03 12:44:53 +0000

    demo pouchdb database created

---
+++ wwwroot/js/createPouchDb.js
@@ -0,0 +1,12 @@
+﻿var db = new PouchDB("mice_database");
+db.bulkDocs([
+    {
+        _id: 'AA123456789',
+        patient_sex: 'F',
+        patient_gene:'Rab15',
+        parameter_name:'body',
+        phenotyping_center:'WTSI',
+        urlString: 'dicomImages/mouse1.dcm'
+    }
+])
+

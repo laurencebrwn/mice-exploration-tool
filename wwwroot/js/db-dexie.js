a1f673857d9195af64b4373a4b53002e297fd24d
Laurence Brown <snowsnooks@gmail.com>
2020-03-06 13:06:17 +0000

    db-dexie js now fully integrated and working with buttons, only a few issues left with asp.net interferring. complex queries to go!

--- wwwroot/js/db-dexie.js
+++ wwwroot/js/db-dexie.js
@@ -5,161 +5,161 @@ var miceTestData = [{
     patient_gene: 'Rab15',
     parameter_name: 'body',
     phenotyping_center: 'WTSI',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse1.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/AA123456789.dcm'
 }, {
     patient_id: 'BB123456789',
     patient_sex: 'F',
     patient_gene: 'Rab15',
     parameter_name: 'head',
     phenotyping_center: 'TCP',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse2.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/BB123456789.dcm'
 }, {
     patient_id: 'CC123456789',
     patient_sex: 'M',
     patient_gene: 'Rab15',
     parameter_name: 'body',
     phenotyping_center: 'ICS',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse3.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/CC123456789.dcm'
 }, {
     patient_id: 'DD123456789',
     patient_sex: 'M',
     patient_gene: 'Rab15',
     parameter_name: 'body',
     phenotyping_center: 'ICS',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse4.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/DD123456789.dcm'
 }, {
     patient_id: 'EE123456789',
     patient_sex: 'M',
     patient_gene: 'Rab15',
     parameter_name: 'paw',
     phenotyping_center: 'TCP',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse5.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/EE123456789.dcm'
 }, {
     patient_id: 'FF123456789',
     patient_sex: 'F',
     patient_gene: 'Rab15',
     parameter_name: 'paw',
     phenotyping_center: 'WTSI',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse6.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/FF123456789.dcm'
 }, {
     patient_id: 'GG123456789',
     patient_sex: 'F',
     patient_gene: 'Rab18',
     parameter_name: 'body',
     phenotyping_center: 'ICS',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse7.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/GG123456789.dcm'
 }, {
     patient_id: 'HH123456789',
     patient_sex: 'F',
     patient_gene: 'Rab15',
     parameter_name: 'head',
     phenotyping_center: 'WTSI',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse8.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/HH123456789.dcm'
 }, {
     patient_id: 'JJ123456789',
     patient_sex: 'F',
     patient_gene: 'Rab15',
     parameter_name: 'head',
     phenotyping_center: 'TCP',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse9.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/JJ123456789.dcm'
 }, {
     patient_id: 'KK123456789',
     patient_sex: 'M',
     patient_gene: 'Rab15',
     parameter_name: 'body',
     phenotyping_center: 'ICS',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse10.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/KK123456789.dcm'
 }, {
     patient_id: 'LL123456789',
     patient_sex: 'M',
     patient_gene: 'Rab15',
     parameter_name: 'body',
     phenotyping_center: 'TCP',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse11.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/LL123456789.dcm'
 }, {
     patient_id: 'MM123456789',
     patient_sex: 'F',
     patient_gene: 'Rab15',
     parameter_name: 'body',
     phenotyping_center: 'TCP',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse12.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/MM123456789.dcm'
 }, {
     patient_id: 'NN123456789',
     patient_sex: 'F',
     patient_gene: 'Rab15',
     parameter_name: 'paw',
     phenotyping_center: 'WTSI',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse13.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/NN123456789.dcm'
 }, {
     patient_id: 'PP123456789',
     patient_sex: 'F',
     patient_gene: 'Rab18',
     parameter_name: 'paw',
     phenotyping_center: 'ICS',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse14.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/PP123456789.dcm'
 }, {
     patient_id: 'RR123456789',
     patient_sex: 'F',
     patient_gene: 'Rab18',
     parameter_name: 'body',
     phenotyping_center: 'ICS',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse15.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/RR123456789.dcm'
 }, {
     patient_id: 'SS123456789',
     patient_sex: 'F',
     patient_gene: 'Rab18',
     parameter_name: 'head',
     phenotyping_center: 'WTSI',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse16.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/SS123456789.dcm'
     }];
 var urlTestData = [{
     patient_id: 'AA123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse1.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/AA123456789.dcm'
 }, {
     patient_id: 'BB123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse2.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/BB123456789.dcm'
 }, {
     patient_id: 'CC123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse3.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/CC123456789.dcm'
 }, {
     patient_id: 'DD123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse4.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/DD123456789.dcm'
 }, {
     patient_id: 'EE123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse5.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/EE123456789.dcm'
 }, {
     patient_id: 'FF123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse6.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/FF123456789.dcm'
 }, {
     patient_id: 'GG123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse7.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/GG123456789.dcm'
 }, {
     patient_id: 'HH123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse8.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/HH123456789.dcm'
 }, {
     patient_id: 'JJ123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse9.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/JJ123456789.dcm'
 }, {
     patient_id: 'KK123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse10.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/KK123456789.dcm'
 }, {
     patient_id: 'LL123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse11.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/LL123456789.dcm'
 }, {
     patient_id: 'MM123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse12.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/MM123456789.dcm'
 }, {
     patient_id: 'NN123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse13.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/NN123456789.dcm'
 }, {
     patient_id: 'PP123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse14.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/PP123456789.dcm'
 }, {
     patient_id: 'RR123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse15.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/RR123456789.dcm'
 }, {
     patient_id: 'SS123456789',
-    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/mouse16.dcm'
+    urlString: 'https://raw.githubusercontent.com/18685030/DicomTestImages/master/dicomImages/SS123456789.dcm'
 }];
 
 function createDb() {
@@ -188,7 +188,7 @@ function queryDb(id_q,sex_q,gene_q,parameter_q,center_q) {
     return db.open().then(function () {
         console.log('patient query in process');
         if (id_q == 'A') { return db.mice.toArray(); }
-        else { return db.mice.where('patient_id').equals(id_q).toArray();}
+        else { return db.mice.where('patient_id').equals(id_q).toArray(); }
     }).then(function (queriedArr) {
         return db.open().then(function () { db.results.clear(); return db.results.bulkAdd(queriedArr); })
     }).then(function () {
@@ -215,12 +215,25 @@ function queryDb(id_q,sex_q,gene_q,parameter_q,center_q) {
         else { return db.results.where('phenotyping_center').equals(center_q).toArray(); }
     }).then(function (queriedArr) {
         return db.open().then(function () { db.results.clear(); return db.results.bulkAdd(queriedArr); })
-    })/*.then(function () {
+    }).then(function () {
+        return clearDivs();
+    }).then(function () {
         return loadDivs();
-    })*/.catch(function (err) {
+    }).catch(function (err) {
         console.error(err.stack || err);
     });
 };
+
+function clearDivs() {
+    //delete current divs
+
+    var x = document.getElementById("images").querySelectorAll(".mouse");
+    console.log("x:",x[0], x[1]);
+    for (i = 0; i < x.length; i++) {
+        x[i].remove();
+        console.log("removed div", i);
+    }
+}
 
 function loadDivs() {
     db.open().then(function () {
@@ -233,6 +246,8 @@ function loadDivs() {
 
         cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
 
+
+
         //Forloop through image set and appends image to end of previous image
 
         for (var i = 0; i < ImageArray.length; i++) {
@@ -243,6 +258,7 @@ function loadDivs() {
             //Create new div element for each DICOM image and insert it befor the dicomImage div in body
 
             const imageDiv = document.createElement("div");
+            imageDiv.className = "mouse"
             imageDiv.style = "width:20vw; height:20vw; border: thin solid white; display:inline-block";
             var currentDiv = document.getElementById("dicomImage");
             currentDiv.parentNode.insertBefore(imageDiv, currentDiv);

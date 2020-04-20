72449592e50509dfe9cdfcddde4b62848222de9d
18685030 <GarryClawson>
2020-04-20 10:41:35 +0100

    Removed unneeded classes

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -325,8 +325,6 @@ namespace miceExplorationTool.Pages
         }
 
 
-
-
         public class Image//Record of images
         {
             private string ID { get; set; }
@@ -507,7 +505,7 @@ namespace miceExplorationTool.Pages
             }
             catch (MySqlException errorMessage) //Prints exception if the connection cannot be opened (wrong password etc)
             {
-                Console.WriteLine("You have not connected to the database: \n\n");
+                Console.WriteLine("You have not connected to the database: \n\n", errorMessage);
 
             }
             finally //Once the try-ctach block is complete the connection is closed
@@ -571,52 +569,6 @@ namespace miceExplorationTool.Pages
         }
 
 
-    }
-
-    //Import the users file path
-
-    public class Image//Record of images
-    {
-        private List<string> ImageFilepaths = new List<string>();//stores the groups of images
-        private List<string> TagHeaders = new List<string>();
-        private List<string> TagInfo = new List<string>();
-
-        public Image()
-        {//creates a new object
-
-        }
-
-        public void AddImage(List<string> filepath)
-        {//sets the list of images to the image
-            ImageFilepaths = filepath;
-        }
-
-        public void AddTagInfo(List<string> headers, List<string> body)
-        {//sets the info as 2 a
-            this.TagHeaders = headers;
-            this.TagInfo = body;
-        }
-
-
-        public List<string> GetImages()
-        {//returns the list of images
-            return this.ImageFilepaths;
-        }
-
-        public List<string> GetHeaders()
-        {
-            return this.TagHeaders;
-        }
-
-        public List<string> GetInfo()
-        {
-            return this.TagInfo;
-        }
-
-        internal void SetImage(List<string> files)
-        {
-            throw new NotImplementedException();
-        }
     }
 
 }
